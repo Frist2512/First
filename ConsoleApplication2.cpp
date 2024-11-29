c2c53b5c679bc9162e6afcc334d0b98ce8f4d26e
Frist2512 <supervana723@gmail.com>
2024-11-29 09:53:38 +0900

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+    
+int main()
+{
+    //переменные
+    int a;
+    std::cout << "vvedite cheslo =";
+   
+    std::cin >> a;
+    
+    int b;
+    std::cout << "vvedite cheslo =";
+
+    std::cin >> b;
+    //какое вычисление хотите выполить 
+    std::cout << (a + b);
+    std::cout << (a / b);
+    std::cout << (a - b);
+ 
+}
+
